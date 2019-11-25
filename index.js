c46f42408d27657fba49c1ec5646de56718c9f0a
Griffin Alcorn <griffin.alcorn@gmail.com>
2019-11-25 16:31:17 -0500

    uses drinksQueries successfully

--- index.js
+++ index.js
@@ -2,19 +2,26 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const db = require("./queries");
+const dbDrinks = require ("./drinksQueries")
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+//GET
 app.get("/", (request, response) => {
   response.json({ info: "Node.js, Express, and Postgres API" });
 });
 app.get("/api/users", db.getUsers);
+app.get("/api/drinks", dbDrinks.getDrinks);
+app.get("/api/drinks-tracking", dbDrinks.getDrinksTracking);
+
+//POST
 app.post("/api/users", db.addUser);
-app.post('/api/login', db.loginUser)
-// Start server
+app.post('/api/login', db.loginUser);
+
+// START SERVER
 app.listen(process.env.PORT || 3002, () => {
   console.log(`Server listening on port:3002`);
 });
